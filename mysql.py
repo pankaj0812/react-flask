7dd7c5c00d92f346e8584cbb5d9ab71621804bc7
Pankaj Singh <UGAM\pankaj.singh@ug-gw-l319.ugamsolutions.com>
2019-10-26 17:12:49 +0530

    Completed CRUD functions for the api

--- mysql.py
+++ mysql.py
@@ -22,6 +22,49 @@ def get_all_employees():
     return jsonify(rv)
 
 
+@app.route('/api/employee', methods=['POST'])
+def add_employee():
+    cur = mysql.connection.cursor()
+    name = request.get_json()['name']
+    department = request.get_json()['department']
+    salary = request.get_json()['salary']
+    insertQuery = """INSERT INTO employee_dashboard.employees (name,department,salary)
+                VALUES
+                (%s, %s, %s)"""
+    record = (name, department, salary)
+    cur.execute(insertQuery, record)
+    mysql.connection.commit()
+    result = {'name':name, 'department': department, 'salary':salary}
+    return jsonify({"result": result})
+
+
+@app.route('/api/employee/<id>', methods=['PUT'])
+def update_employee_details(id):
+    cur = mysql.connection.cursor()
+    name = request.get_json()['name']
+    department = request.get_json()['department']
+    salary = request.get_json()['salary']
+    updateQuery = """UPDATE employee_dashboard.employees SET name = %s, department= %s, salary = %s where id = %s"""
+    record = (name, department, salary, id)
+    cur.execute(updateQuery, record)
+    mysql.connection.commit()
+    result = {'name':name, 'department': department, 'salary':salary}
+    return jsonify({"result": result})
+
+
+@app.route('/api/employee/<id>', methods=['DELETE'])
+def delete_employee_record(id):
+    cur = mysql.connection.cursor()
+    deleteQuery = """DELETE FROM employee_dashboard.employees WHERE id = %s"""
+    record = (id)
+    response = cur.execute(deleteQuery, record)
+    mysql.connection.commit()
+
+    if response > 0:
+        result = {'message': 'record deleted'}
+    else:
+        result = {'message': 'no record found'}
+    return jsonify({"result": result})
 
 
 if __name__ == '__main__':
